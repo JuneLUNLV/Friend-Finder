35fc9a53004d1b224fd93077b7f4e1f7620dc419
June Liang <liangj10@unlv.nevada.edu>
2022-07-07 17:34:04 -0700

    type check

--- src/utils/findPersonByUserName.ts
+++ src/utils/findPersonByUserName.ts
@@ -1,12 +1,13 @@
 import { Person } from "../types/Person";
 
 const findPersonByUsername = (clickedId:string,peopleList:Array<Person>) => {
+  let returnPerson = peopleList[0];
     for (let person of peopleList){
       if(person.login.username == clickedId){
-        return(person)
+        returnPerson = person
       }
     }
-    return null;
+    return returnPerson;
 }
 
 export default findPersonByUsername;
